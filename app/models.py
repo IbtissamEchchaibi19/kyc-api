d8482146eec661f76872ab2f63df86c97200c433
btissam19 <btissam1234btissam12@gmail.com>
2024-08-02 13:47:29 +0100

    adding expired link

--- app/models.py
+++ app/models.py
@@ -198,3 +198,30 @@ def get_all_client_emails():
     clients_collection = mongo.db.clients
     clients = clients_collection.find({}, {"email": 1, "_id": 0})
     return [client["email"] for client in clients]
+
+
+
+
+def save_token_to_db(email, token, used=False):
+    tokens_collection = mongo.db.tokens
+    result = tokens_collection.update_one(
+        {'email': email, 'token': token},
+        {'$set': {'used': used}},
+        upsert=True
+    )
+    print(f"MongoDB update result: {result.raw_result}")  # Debug print statement
+    return result.modified_count > 0
+
+def is_token_used(token):
+    tokens_collection = mongo.db.tokens
+    token_entry = tokens_collection.find_one({'token': token})
+    return token_entry is not None and token_entry.get('used', False)
+
+def mark_token_as_used(token):
+    tokens_collection = mongo.db.tokens
+    result = tokens_collection.update_one(
+        {'token': token},
+        {'$set': {'used': True}}
+    )
+    print(f"MongoDB mark as used result: {result.raw_result}")  # Debug print statement
+    return result.modified_count > 0
